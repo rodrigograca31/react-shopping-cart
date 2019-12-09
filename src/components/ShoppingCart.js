873a0bb201d85297113836d812044b3efb23e0b1
rodrigograca31 <rodrigograca1993@gmail.com>
2019-12-09 14:13:44 +0000

    STEP 7 - The final stretch

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
+import CartContext from "../contexts/CartContext";
 
 // Components
 import Item from "./ShoppingCartItem";
 
-const ShoppingCart = props => {
+const ShoppingCart = () => {
+	const cart = useContext(CartContext);
+
 	const getCartTotal = () => {
-		return props.cart
+		return cart
 			.reduce((acc, value) => {
 				return acc + value.price;
 			}, 0)
@@ -14,7 +17,7 @@ const ShoppingCart = props => {
 
 	return (
 		<div className="shopping-cart">
-			{props.cart.map(item => (
+			{cart.map(item => (
 				<Item key={item.id} {...item} />
 			))}
 
